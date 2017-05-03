5e95d2258f15e602ceed783d8d06db14bb2e2f8e
William Horton <wdt.horton@gmail.com>
2017-05-03 09:45:22 -0400

    Handle comments correctly.

--- src/helpers/transformProperties.js
+++ src/helpers/transformProperties.js
@@ -1,11 +1,7 @@
 import propTypeToFlowType from './propTypeToFlowType';
 
 export default function transformProperties(j, properties) {
-  return properties.map(property => {
-    const type = propTypeToFlowType(j, property.key, property.value);
-    type.value.leadingComments = property.leadingComments;
-    type.value.comments = property.comments;
-    return type;
-  });
+  return properties.map(property =>
+    propTypeToFlowType(j, property.key, property.value, property.leadingComments, property.comments)
+  );
 }
-
